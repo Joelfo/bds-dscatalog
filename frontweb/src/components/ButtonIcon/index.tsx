9aea2d8f4a862ea3af7f0b92587b00518f8462cc
Joel Lopes <joellopessouza@id.uff.br>
2023-01-17 23:49:43 -0300

    home styling

---
+++ frontweb/src/components/ButtonIcon/index.tsx
@@ -0,0 +1,10 @@
+const ButtonIcon = () => {
+
+    return (
+        <button className="btn btn-primary btn-icon">
+            <h6>Inicie agora a sua busca</h6>
+        </button>
+    )
+}
+
+export default ButtonIcon;
