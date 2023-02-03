add79b740c919f5253a7d193fc66b74df25940ca
Joel Lopes <joellopessouza@id.uff.br>
2023-02-02 22:17:01 -0300

    backend setup

--- frontweb/src/components/ButtonIcon/index.tsx
+++ frontweb/src/components/ButtonIcon/index.tsx
@@ -1,10 +1,18 @@
-const ButtonIcon = () => {
+import "./styles.css";
+import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
 
-    return (
-        <button className="btn btn-primary btn-icon">
-            <h6>Inicie agora a sua busca</h6>
+const ButtonIcon = () => {
+  return (
+    <div className="btn-container">
+        <button className="btn btn-primary">
+          <h6>Inicie agora sua busca</h6>
         </button>
-    )
-}
+      <div className="btn-icon-container">
+        <ArrowIcon/>
+      </div>
+    </div>
+    );
+  }
+
 
 export default ButtonIcon;
